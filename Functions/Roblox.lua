808a40bdb27b963c46718fef81aab76e397d5aca
Jinn0x0 <138560080+Jinn0x0@users.noreply.github.com>
2025-09-16 05:24:23 -0700

    x

--- Functions/Roblox.lua
+++ Functions/Roblox.lua
@@ -3,10 +3,16 @@ loadstring(HttpService:GetAsync("https://raw.githubusercontent.com/Jinn0x0/Suji/
 
 local M = {}
 
-function M.printPlayersName()
-    for _, plr in pairs(Players:GetPlayers()) do
-        print(plr.Name)
-    end
+function M.predictWithAngularVelocity(p0, v, rot0, angVel, t, maxDist)
+	local linearPos = p0 + v * t;
+	local futureYaw = rot0 + angVel.Y * t;
+	local dir2D = Vector3.new(math.cos(futureYaw), 0, math.sin(futureYaw));
+	local predicted = linearPos + dir2D;
+	local dist = (predicted - p0).Magnitude;
+	if dist > maxDist then
+		predicted = p0 + ((predicted - p0)).Unit * maxDist;
+	end;
+	return predicted;
 end
 
 return M
