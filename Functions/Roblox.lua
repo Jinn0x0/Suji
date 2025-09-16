3feba16541e1ec0b2415cdb8330ca66dd42eb0eb
Jinn0x0 <138560080+Jinn0x0@users.noreply.github.com>
2025-09-16 10:36:36 +0000

    modified:   Functions/Roblox.lua

--- Functions/Roblox.lua
+++ Functions/Roblox.lua
@@ -1,3 +1,9 @@
-require("Bin/GetService")
+local M = {}
 
-print(Players.LocalPlayer)
+function M.printPlayersName()
+    for _, plr in pairs(Players:GetPlayers()) do
+        print(plr.Name)
+    end
+end
+
+return M
