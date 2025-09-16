8d969799b6494e8c068de7bddbdb56fec65a2941
Jinn0x0 <138560080+Jinn0x0@users.noreply.github.com>
2025-09-16 11:44:44 +0000

    modified:   Functions/Roblox.lua

--- Functions/Roblox.lua
+++ Functions/Roblox.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/%s/Suji/main/%s/%s"):format("Jinn0x0", "Bin", "GetService.lua")))() -- Load all services
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jinn0x0/Suji/main/Bin/GetService.lua"))() -- Load all services
 
 function M.printPlayersName()
     for _, plr in pairs(Players:GetPlayers()) do
