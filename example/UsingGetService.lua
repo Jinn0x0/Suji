789f805b2de237bc3d0d7ea600e8d96696e8b9aa
Jinn0x0 <138560080+Jinn0x0@users.noreply.github.com>
2025-09-16 10:17:51 +0000

    modified:   example/UsingGetService.lua

--- example/UsingGetService.lua
+++ example/UsingGetService.lua
@@ -2,7 +2,11 @@ local owner = "Jinn0x0"
 local branch = "Bin"
 
 local function webImport(file)
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/%s/Suji/refs/heads/main/%s/%s.lua"):format(owner, branch, file))()
+    return loadstring(
+        game:HttpGet(
+            ("https://raw.githubusercontent.com/%s/Suji/refs/heads/main/%s/%s.lua"):format(owner, branch, file)
+        )
+    )()
 end
 
 webImport("GetService")
