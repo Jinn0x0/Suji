e12cb4192fa766235202cf252763cbb0b8eed350
Jinn0x0 <138560080+Jinn0x0@users.noreply.github.com>
2025-09-16 12:14:17 +0000

    modified:   example/UsingGetService.lua

--- example/UsingGetService.lua
+++ example/UsingGetService.lua
@@ -3,7 +3,7 @@ local branch = "Bin"
 
 local function webImport(file)
     return loadstring(
-        game:HttpGet(
+        game:HttpService:GetAsync(
             ("https://raw.githubusercontent.com/%s/Suji/main/%s/%s.lua"):format(owner, branch, file)
         )
     )()
