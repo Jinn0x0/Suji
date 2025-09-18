50475843823dfecc2ea1307344bdb3c94000e370
Jinn0x0 <138560080+Jinn0x0@users.noreply.github.com>
2025-09-18 05:42:13 +0000

    modified:   Bin/Test.lua

--- Bin/Test.lua
+++ Bin/Test.lua
@@ -4,4 +4,17 @@ function M.run()
     print("Hello from Test.lua!")
 end
 
+function M.TestGetNameFromId(userId)
+    local success, username = pcall(function()
+        return Players:GetNameFromUserIdAsync(userId)
+    end)
+
+    if success then
+        print("Username is: " .. username)
+    else
+        warn("Failed to get username: " .. tostring(username))
+    end
+end
+
+
 return M
