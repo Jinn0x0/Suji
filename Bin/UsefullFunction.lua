0025da1a5f7d9b5cf477f1818ec238a36f10f7ff
Jinn0x0 <138560080+Jinn0x0@users.noreply.github.com>
2025-09-18 06:11:33 +0000

    modified:   Bin/UsefullFunction.lua

--- Bin/UsefullFunction.lua
+++ Bin/UsefullFunction.lua
@@ -4,6 +4,16 @@ function M.getDistance(object_one, object_two)
   return (object_one.Position - object_two.Position).Magnitude
 end
 
+function M.sign(num)
+  if num == 0 then
+    return 0
+  elseif num > 0 then
+    return 1
+  else
+    return -1
+  end
+end
+
 function M.round(num, place)
   local mult = 10 ^ place
   return math.floor(num * mult + 0.5) / mult
